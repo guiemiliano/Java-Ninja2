88e02be91df901ebbf5f4b00392741dcc1d4133f
guiemiliano <guiemiliano.dev@gmail.com>
2024-12-10 20:11:13 -0300

    Novas Implementações!

--- src/NinjaRecord.java
+++ src/NinjaRecord.java
@@ -2,9 +2,9 @@
 
     O Record em Java foi introduzido no Java 14 como uma maneira simples e concisa
     de declarar classes imutáveis que servem para conter dados. Ele faz parte do
-     pacote java.lang e é projetado para reduzir o código boilerplate associado a
-      classes que simplesmente armazenam dados, como getters, setters, equals(),
-      hashCode(), e toString().
+    pacote java.lang e é projetado para reduzir o código boilerplate associado a
+    classes que simplesmente armazenam dados, como getters, setters, equals(),
+    hashCode(), e toString().
  */
 public record NinjaRecord(String nome, String email, int numero) {
 
@@ -13,9 +13,9 @@ public record NinjaRecord(String nome, String email, int numero) {
     }
 
 }
-
-
-
 //Todos os atributos são "final" por padrão
 //Records não tem Setter, não da para alterar
 //não precisa usar getter
+//Usar para coisas que não necessitam de tanta mudança
+
+
